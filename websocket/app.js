e22e74b22bf460f83a3a140baf06d2b9ae1554c8
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-21 14:04:26 +0300

    websocket

---
+++ websocket/app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const socketio = require('socket.io');
+const server = http.createServer((req, res) => {
+    res.end('hey!');
+});
+
+server.listen(3000);
+
+const io = socketio.listen(server);
+
+io.sockets.on('connection', (socket) => {
+    console.log('Kullanıcı bağlandı.');
+
+    socket.on('disconnect', () => {
+        console.log('Kullanıcı ayrıldı.');
+    });
+});
