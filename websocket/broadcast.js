194d6d1b28dec21e37f813a0c7603021f2fe4908
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-22 18:06:21 +0300

    broad cast & socketio namespaces and rooms pdf

--- websocket/broadcast.js
+++ websocket/broadcast.js
@@ -11,13 +11,8 @@ const io = socketio.listen(server);
 io.sockets.on('connection', (socket) => {
     console.log('Kullanıcı bağlandı.');
 
-    /*setInterval(() => {
-        socket.emit('merhaba de', { country: 'Türkiye' });
-    }, 1000);*/
-
-    socket.on('selam ver', (data) => {
-        console.log(data.city);
-        console.log(data.name);
+    socket.on('new-user', (data) => {
+       socket.broadcast.emit('user', { name: data.name });
     });
 
     socket.on('disconnect', () => {
