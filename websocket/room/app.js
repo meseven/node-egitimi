4549eff02fa8caee40981d4b2ea7485a5446d78c
Mehmet Seven <mehmetseven0@gmail.com>
2017-12-23 12:32:07 +0300

    rooms

--- websocket/room/app.js
+++ websocket/room/app.js
@@ -9,9 +9,20 @@ const io = socketio.listen(server);
 io.on('connection', (socket) => {
     socket.on('joinRoom', (data) => {
        socket.join(data.name, () => {
-           let count = io.sockets.adapter.rooms[data.name].length;
-           io.to(data.name).emit('new join', { count });
+           io.to(data.name).emit('new join', { count: getOnlineCount(io, data) });
            socket.emit('log',  { message: 'Odaya girdiniz.' });
        });
     });
+
+    socket.on('leaveRoom', (data) => {
+        socket.leave(data.name, () => {
+            io.to(data.name).emit('leavedRoom', { count: getOnlineCount(io, data) });
+            socket.emit('socket.leaved', { message: 'Odadan ayrıldınız.' });
+        });
+    });
 });
+
+const getOnlineCount = (io, data) => {
+    const room = io.sockets.adapter.rooms[data.name];
+    return room ? room.length : 0;
+};
